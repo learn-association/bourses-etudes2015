0c359c53f6805a401644f7979fe58f03fdf3afd7
gagarine <simon@antistatique.net>
2015-05-24 13:54:40 +0200

    first graph

--- app/app.js
+++ app/app.js
@@ -2,18 +2,25 @@
 
     // ----- Init -----
 
-    var app = angular.module("app", ["chart.js", 'ui.bootstrap']);
+    var app = angular.module("app", ["chart.js", 'ui.bootstrap', 'pascalprecht.translate']);
 
-    app.config(function (ChartJsProvider) {
+    app.config(function (ChartJsProvider, $translateProvider) {
         // Configure all charts
         ChartJsProvider.setOptions({
             colours: ['#97BBCD', '#DCDCDC', '#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
-            responsive: true
+            responsive: true,
+            animationEasing: "easeOutQuart"
         });
         // Configure all doughnut charts
         ChartJsProvider.setOptions('Doughnut', {
             animateScale: true
         });
+
+        $translateProvider.translations('en', {});
+        $translateProvider.translations('fr', {});
+        $translateProvider.translations('de', {});
+        $translateProvider.preferredLanguage('en');
+
     });
 
     // ----- Services -----
@@ -36,32 +43,72 @@
 
     // ----- Controllers -----
 
-    app.controller("UserController", function ($scope, userService) {
+    app.controller("UserController", function ($scope, userService, $translate) {
         var user = this;
         user.getVoteState = function () {
             return userService.voteState;
         };
-        user.setVoteToYes = function () {
-            userService.voteState = true;
+        $scope.voteState = userService.voteState;
+
+        $scope.$watch(function ($scope) {
+                return $scope.voteState
+            },
+            function () {
+                userService.voteState = $scope.voteState;
+            }
+        );
+
+        $scope.changeLanguage = function (key) {
+            $translate.use(key);
         };
-        user.setVoteToNo = function () {
-            userService.voteState = false;
-        }
     });
 
 
-    app.controller("DoughnutPIBController", function ($scope, $rootScope, userService) {
-        $scope.labels = ["Autres", "Formation"];
+    app.controller("ChartPIBController", function ($scope, $rootScope, userService) {
+        $scope.labels = ["Autres", "Education"];
         $scope.getData = function () {
             if (userService.voteState) {
-                $scope.data = [95, 5];
+                $scope.investment = 34662;
+                // 5.54962 %
+                $scope.data = [94.45, 5.55];
             } else {
-                $scope.data = [95, 6];
+                $scope.investment = 34762;
+                // 5.5656312 %
+                $scope.data = [94.43, 5.57];
             }
         };
         $scope.data = $scope.getData();
 
         $rootScope.$on('userService:voteStateChanged', $scope.getData);
     });
+    app.controller("ChartPublicInvestmentController", function ($scope, $rootScope, userService) {
+        $scope.labels = ["Autres", "Education"];
+        $scope.getData = function () {
+            if (userService.voteState) {
+                $scope.data = [85, 17];
+            } else {
+                $scope.data = [84, 18];
+            }
+        };
+        $scope.data = $scope.getData();
+
+        $rootScope.$on('userService:voteStateChanged', $scope.getData);
+    });
+
+    app.controller("ChartPublicInvestmentDetailController", function ($scope, $rootScope, userService) {
+        $scope.labels = ["Autres","Autres","Autres", "Bourse"];
+        $scope.getData = function () {
+            if (userService.voteState) {
+                $scope.data = [10,40, 85, 1];
+            } else {
+                $scope.data = [10,40, 85, 2];
+            }
+        };
+        $scope.data = $scope.getData();
+
+        $rootScope.$on('userService:voteStateChanged', $scope.getData);
+    });
+
+
 
 })();
